4366b26a5bbf97423e7e86a8449aadd07e0e2e0a
shiyi <shiyi@zugeliang01.com>
2019-08-21 17:04:57 +0800

    no message

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -93,8 +93,9 @@ module.exports = {
         let req = ctx.request.body;
         let userId = req.userId? req.userId: ctx.state.userId;
         try {
-            console.log(User, '=!!doc__!!===')
+            console.log(userId, '=!!doc__!!===')
             let data = await User.findOne({_id: userId});
+            console.log(data, '=!!doc__!!===')
             let message = await Remarks.find({status: 1, toUid: ctx.state.userId});
             data._doc.isFans = Utils.getIsStatus(data, 'isFans', ctx.state.userId);
             data._doc.message = message.length;
