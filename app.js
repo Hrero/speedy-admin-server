9ccc8ba631a34657bcfb026add2f70ca86e245a0
shiyi <shiyi@zugeliang01.com>
2019-09-10 14:13:21 +0800

    添加redis mq ws

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const router = require('./server/router/router');
 const logUtil = require("./server/config/log");
 const fetch = require('./server/proxy/fetch');
 const socket = require('./server/function/socket');
+const schedule = require('./server/function/schedule');
 const app = new Koa();
 
-require('./server/db/db')
+require('./server/db/db');
+schedule.clearLogs();
+
 app.context.fetch = fetch; // 全局http请求方法
 
 app.use(koaBody({ // 配置文件上传
@@ -21,7 +24,6 @@ app.use(koaBody({ // 配置文件上传
 }));
 
 app.use(serve(__dirname + "/dist",{ extensions: ['html']}));
-
 app.use(async (ctx, next) => {
     const start = new Date(); // 响应开始时间
     var ms; // 响应间隔时间
